92741c51d57294637ce00f54e87cc6934e4a891b
Jan Wagner <info@janwagner-design.de>
2021-12-12 17:42:11 +0100

    added new favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,6 +38,13 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <title>IOTABOTS</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+        <link rel="manifest" href="favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#20c593" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Web3ReactProvider getLibrary={getLibrary}>
 
