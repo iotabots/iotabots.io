cfde7db0e4672655472a1a73267e973366880f66
huhn511 <sehe89@gmail.com>
2021-12-27 09:30:37 +0100

    add react-reveal effects

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Box, Button, Typography } from '@iotabots/components'
+import Fade from 'react-reveal/Zoom'
 import BackgroundVideo from './BackgroundVideo'
 
 const Hero: React.FC = () => {
@@ -9,51 +10,49 @@ const Hero: React.FC = () => {
   }
   return (
     <Box>
-      <BackgroundVideo blur={0}
-        videoSource='assets/TEASER.mp4'>
+      <BackgroundVideo blur={0} videoSource='assets/TEASER.mp4'>
         <Box className='content'>
-          <Box
-            className='sub-content'
-            sx={{
-              px: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography
-              variant='h2'
-              align='center'
-              color='#ffffff'
-              gutterBottom
-              sx={{ display: { xs: 'none', sm: 'block' } }}
+          <Fade bottom>
+            <Box
+              className='sub-content'
+              sx={{
+                px: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
             >
-              IOTABOTS
-            </Typography>
-
-            <Typography variant='h5'
-              align='center'
-              color='#ffffff'
-              paragraph>
-              First PFP NFTs on IOTA
-            </Typography>
-
-            <Box sx={{ textAlign: 'center' }}>
-              <Button
-                color='secondary'
-                variant='contained'
-                onClick={scrollToBottom}
-                size='large'
-                sx={{
-                  color: 'primary.contrastText',
-                  mt: { sm: 2 },
-                  boxShadow: 2,
-                }}
+              <Typography
+                variant='h2'
+                align='center'
+                color='#ffffff'
+                gutterBottom
+                sx={{ display: { xs: 'none', sm: 'block' } }}
               >
-                Let&apos;s go!
-              </Button>
+                IOTABOTS
+              </Typography>
+
+              <Typography variant='h5' align='center' color='#ffffff' paragraph>
+                First PFP NFTs on IOTA
+              </Typography>
+
+              <Box sx={{ textAlign: 'center' }}>
+                <Button
+                  color='secondary'
+                  variant='contained'
+                  onClick={scrollToBottom}
+                  size='large'
+                  sx={{
+                    color: 'primary.contrastText',
+                    mt: { sm: 2 },
+                    boxShadow: 2,
+                  }}
+                >
+                  Let&apos;s go!
+                </Button>
+              </Box>
             </Box>
-          </Box>
+          </Fade>
         </Box>
       </BackgroundVideo>
     </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Box, Container, ContentBox, Typography } from '@iotabots/components'
+import Fade from 'react-reveal/Zoom'
 import BaseLayout from '../layout/BaseLayout'
 import Hero from '../components/Hero'
 import IotabotGrid from '../components/IotabotGrid'
@@ -12,28 +13,34 @@ export const Home = (): JSX.Element => (
       <Box py={8} bgcolor='background.default'>
         <span id='what' className='anchor' style={{ marginTop: '-100px' }} />
         <Container>
-          <ContentBox
-            image='http://assets.iotabots.io/9.png'
-            // eslint-disable-next-line max-len
-            text='An IOTABOT is a NFT in the form of a profile picture (PFP) that gives you access to the IOTABOTVERSE. At this time they are NFTs (ERC721 Tokens) on the public IOTA Smart Contract EVM Testnet.'
-            headline='WHAT IS AN IOTABOT?!'
-          />
-          <ContentBox
-            image='http://assets.iotabots.io/1000.png'
-            // eslint-disable-next-line max-len
-            text='The IOTABOTVERSE consists of many NFT projects. Discover the metaverse and play, trade and earn with IOTABOTS and others.'
-            headline='WHAT IS the IOTABOTVERSE?!'
-          />
+          <Fade bottom>
+            <ContentBox
+              image='http://assets.iotabots.io/9.png'
+              // eslint-disable-next-line max-len
+              text='An IOTABOT is a NFT in the form of a profile picture (PFP) that gives you access to the IOTABOTVERSE. At this time they are NFTs (ERC721 Tokens) on the public IOTA Smart Contract EVM Testnet.'
+              headline='WHAT IS AN IOTABOT?!'
+            />
+          </Fade>
+          <Fade bottom>
+            <ContentBox
+              image='http://assets.iotabots.io/1000.png'
+              // eslint-disable-next-line max-len
+              text='The IOTABOTVERSE consists of many NFT projects. Discover the metaverse and play, trade and earn with IOTABOTS and others.'
+              headline='WHAT IS the IOTABOTVERSE?!'
+            />
+          </Fade>
         </Container>
       </Box>
 
       <Box py={8} bgcolor='background.light'>
-        <Container maxWidth='md'>
-          <Typography variant='h3' align='center' sx={{ pb: 6 }}>
-            Available Bots
-          </Typography>
-          <IotabotGrid />
-        </Container>
+        <Fade bottom>
+          <Container maxWidth='md'>
+            <Typography variant='h3' align='center' sx={{ pb: 6 }}>
+              Available Bots
+            </Typography>
+            <IotabotGrid />
+          </Container>
+        </Fade>
       </Box>
     </Box>
   </BaseLayout>
