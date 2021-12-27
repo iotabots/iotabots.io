b9491013cf0a22e094f474b49492f50e87fecb0b
huhn511 <sehe89@gmail.com>
2021-12-27 16:55:43 +0100

    add iotabots ui compontents

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,20 +1,32 @@
 import fs from 'fs'
 import matter from 'gray-matter'
 import * as React from 'react'
-import styles from '../styles/Projects.module.scss'
+import { Box, Container, Typography } from '@iotabots/components'
 import Card from '../components/Card'
 import { ProjectMeta } from '../interfaces/project'
+import BaseLayout from '../layout/BaseLayout'
 
 interface IProps {
   projects: ProjectMeta[]
 }
 
 const Projects: React.FC<IProps> = ({ projects }) => (
-  <div className={styles.container}>
-    {projects.map((project) => (
-      <Card key={project.slug} project={project} />
-    ))}
-  </div>
+  <BaseLayout>
+    <Box py={6} display='flex' alignItems='center'>
+      <Container maxWidth='md'>
+        <Typography gutterBottom variant='h1'>
+          Projects
+        </Typography>
+        <Typography variant='body1'>
+          The IOTABOTS Team and the Community building amazing projects.
+          Discover all projects in the IOTABOTS Metaverse.
+        </Typography>
+        {projects.map((project) => (
+          <Card key={project.slug} project={project} />
+        ))}
+      </Container>
+    </Box>
+  </BaseLayout>
 )
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
