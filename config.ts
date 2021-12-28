ff87bcc4f37b5e90d59798e29850738202b54c31
huhn511 <sehe89@gmail.com>
2021-12-28 14:28:01 +0100

    add metatags

---
+++ config.ts
@@ -0,0 +1,13 @@
+export const SEO = {
+  title: 'IOTABOTS',
+  description: 'My description',
+  openGraph: {
+    type: 'website',
+    url: 'www.iotabots.io',
+    title: 'IOTABOTS',
+    description:
+      // eslint-disable-next-line max-len
+      'First NFT Profile Picture Project on IOTA. #DeFi #NFT #GameFi #play2earn',
+    image: '/assets/logo192.png',
+  },
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import { AppProps } from 'next/dist/shared/lib/router/router'
 import ReactGA from 'react-ga'
 import { Navigation } from '../components/Navigation/Navigation'
 import { Footer } from '../components/Footer'
+import { SEO } from '../config'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getLibrary = (provider: any): Web3Provider => {
@@ -31,7 +32,7 @@ const App: React.FC<AppProps> = (props) => {
   return (
     <>
       <Head>
-        <title>IOTABOTS</title>
+        <title>{SEO.title}</title>
         <meta name='viewport' content='initial-scale=1, width=device-width' />
         <link rel='apple-touch-icon' sizes='180x180' href='favicon.ico' />
         <link rel='icon' type='image/png' sizes='32x32' href='favicon.ico' />
@@ -40,12 +41,24 @@ const App: React.FC<AppProps> = (props) => {
         <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#20c593' />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#ffffff' />
+        <meta key='description' name='description' content={SEO.description} />
+        <meta key='og:type' name='og:type' content={SEO.openGraph.type} />
+        <meta key='og:title' name='og:title' content={SEO.openGraph.title} />
+        <meta
+          key='og:description'
+          name='og:description'
+          content={SEO.openGraph.description}
+        />
+        <meta key='og:url' name='og:url' content={SEO.openGraph.url} />
+        <meta key='og:image' name='og:image' content={SEO.openGraph.image} />
         <script
           async
+          // eslint-disable-next-line max-len
           src={`https://www.googletagmanager.com/gtag/js?id=${YOUR_TRACKING_ID}`}
         />
         <script
           async
+          // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
