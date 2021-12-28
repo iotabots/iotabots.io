e31773f029f7bbcf16e7661ac8c6e3491932d558
huhn511 <sehe89@gmail.com>
2021-12-28 15:43:39 +0100

    fetch bot metadata

--- pages/bots/[pid].tsx
+++ pages/bots/[pid].tsx
@@ -10,50 +10,55 @@ import {
 import { useRouter } from 'next/router'
 import BaseLayout from '../../layout/BaseLayout'
 
-const BotDetail: React.FC = () => {
-  const router = useRouter()
+const BotDetail: React.FC<any> = ({ bot }) => (
+  <BaseLayout>
+    <Box>
+      <Container maxWidth='sm'>
+        <Box sx={{ my: 8 }}>
+          <Typography variant='h4' gutterBottom>
+            IOTABOT #{bot.edition}
+          </Typography>
+          <Box pt={8}>
+            <Card
+              sx={{
+                justifyContent: 'center',
+                textAlign: 'center',
 
-  console.log('router', router.query)
-  const { pid } = router.query
-
-  return (
-    <BaseLayout>
-      <Box>
-        <Container maxWidth='sm'>
-          <Box sx={{ my: 8 }}>
-            <Typography variant='h4' gutterBottom>
-              IOTABOT #{pid}
-            </Typography>
-            <Box pt={8}>
-              <Card
+                minHeight: '200px',
+                maxHeight: '400px',
+                minWidth: '200px',
+                maxWidth: '400px',
+              }}
+            >
+              <CardMedia
                 sx={{
-                  justifyContent: 'center',
-                  textAlign: 'center',
-
-                  minHeight: '200px',
-                  maxHeight: '400px',
-                  minWidth: '200px',
-                  maxWidth: '400px',
+                  height: '200px',
                 }}
-              >
-                <CardMedia
-                  sx={{
-                    height: '200px',
-                  }}
-                  // eslint-disable-next-line max-len
-                  image={`https://assets.iotabots.io/compressed/${pid}.png`}
-                />
-                <CardContent />
-                <Typography gutterBottom variant='body1'>
-                  {`IOTABOT #${pid}`}
-                </Typography>
-              </Card>
-            </Box>
+                // eslint-disable-next-line max-len
+                image={`https://assets.iotabots.io/compressed/${bot.edition}.png`}
+              />
+              <CardContent />
+              <Typography gutterBottom variant='body1'>
+                {`IOTABOT #${bot.edition}`}
+              </Typography>
+            </Card>
           </Box>
-        </Container>
-      </Box>
-    </BaseLayout>
-  )
-}
+        </Box>
+      </Container>
+    </Box>
+  </BaseLayout>
+)
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export async function getServerSideProps({ params }) {
+  if (Number(params.pid)) {
+    const res = await fetch(`https://assets.iotabots.io/${params.pid}.json`)
+    const data = await res.json()
 
+    return {
+      props: { bot: data },
+    }
+  }
+
+  return { props: params }
+}
 export default BotDetail
