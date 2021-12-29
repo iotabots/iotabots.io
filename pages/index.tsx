bec6c3b2e6816793b1374d7b462b1a8069f1d842
huhn511 <sehe89@gmail.com>
2021-12-29 14:14:53 +0100

    add project section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import fs from 'fs'
+import matter from 'gray-matter'
 import {
   Box,
   Button,
   Container,
   ContentBox,
+  Grid,
   Typography,
 } from '@iotabots/components'
 import Fade from 'react-reveal/Zoom'
@@ -11,12 +14,16 @@ import { useRouter } from 'next/router'
 import BaseLayout from '../layout/BaseLayout'
 import Hero from '../components/Hero'
 import IotabotGrid from '../components/IotabotGrid'
+import ProjectCard from '../components/ProjectCard'
 
-export const Home = (): JSX.Element => {
+export const Home = ({ projects }): JSX.Element => {
   const router = useRouter()
 
   const goToBots = (): void => {
     router.push('/bots')
+  }
+  const goToProjects = (): void => {
+    router.push('/projects')
   }
   return (
     <BaseLayout>
@@ -92,9 +99,78 @@ export const Home = (): JSX.Element => {
             </Box>
           </Fade>
         </Box>
+
+        <Box py={8} bgcolor='background.default'>
+          <Fade bottom>
+            <Container maxWidth='md'>
+              <Typography variant='h3' align='center' sx={{ pb: 6 }}>
+                More Projects
+              </Typography>
+              <Typography variant='body1' align='center' sx={{ pb: 6 }}>
+                There are serveal projects around IOTABOTS - Discover the
+                Metaverse!
+              </Typography>
+            </Container>
+            <Grid
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}
+              container
+              spacing={{ xs: 1, md: 1 }}
+              columns={{ xs: 12, sm: 12, md: 12 }}
+            >
+              {projects.slice(1, 3).map((project) => (
+                <Grid
+                  item
+                  key={project.slug}
+                  style={{
+                    maxWidth: '33%',
+                    minWidth: '360px',
+                  }}
+                >
+                  <ProjectCard key={project.slug} project={project} />
+                </Grid>
+              ))}
+              <Button
+                variant='contained'
+                onClick={goToProjects}
+                size='large'
+                sx={{
+                  color: 'primary.contrastText',
+                  mt: { sm: 2 },
+                  boxShadow: 2,
+                }}
+              >
+                Discover All Proects
+              </Button>
+            </Grid>
+          </Fade>
+        </Box>
       </Box>
     </BaseLayout>
   )
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export async function getStaticProps() {
+  const files = fs.readdirSync('projects')
+
+  const projects = files.map((file) => {
+    const data = fs.readFileSync(`projects/${file}`).toString()
+
+    return {
+      ...matter(data).data,
+      slug: file.split('.')[0],
+    }
+  })
+
+  return {
+    props: {
+      projects,
+    },
+  }
 }
 
 export default Home
