3e0220f14c09edec16ac5734f1f9d38978e7dd87
huhn511 <sehe89@gmail.com>
2021-12-29 16:13:20 +0100

    set button to center

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,42 +110,44 @@ export const Home = ({ projects }): JSX.Element => {
                 There are serveal projects around IOTABOTS - Discover the
                 Metaverse!
               </Typography>
-            </Container>
-            <Grid
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-              }}
-              container
-              spacing={{ xs: 1, md: 1 }}
-              columns={{ xs: 12, sm: 12, md: 12 }}
-            >
-              {projects.slice(1, 3).map((project) => (
-                <Grid
-                  item
-                  key={project.slug}
-                  style={{
-                    maxWidth: '33%',
-                    minWidth: '360px',
+              <Grid
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                }}
+                container
+                spacing={{ xs: 1, md: 1 }}
+                columns={{ xs: 12, sm: 12, md: 12 }}
+              >
+                {projects.slice(1, 3).map((project) => (
+                  <Grid
+                    item
+                    key={project.slug}
+                    style={{
+                      maxWidth: '33%',
+                      minWidth: '360px',
+                    }}
+                  >
+                    <ProjectCard key={project.slug} project={project} />
+                  </Grid>
+                ))}
+              </Grid>
+              <Box display='flex' alignItems='center' justifyContent='center'>
+                <Button
+                  variant='contained'
+                  onClick={goToProjects}
+                  size='large'
+                  sx={{
+                    color: 'primary.contrastText',
+                    mt: { sm: 2 },
+                    boxShadow: 2,
                   }}
                 >
-                  <ProjectCard key={project.slug} project={project} />
-                </Grid>
-              ))}
-              <Button
-                variant='contained'
-                onClick={goToProjects}
-                size='large'
-                sx={{
-                  color: 'primary.contrastText',
-                  mt: { sm: 2 },
-                  boxShadow: 2,
-                }}
-              >
-                Discover All Proects
-              </Button>
-            </Grid>
+                  Discover All Proects
+                </Button>
+              </Box>
+            </Container>
           </Fade>
         </Box>
       </Box>
