1fefb0879552aecc9cc517cb61d146b992e40f86
r3p0x <svreiser@gmail.com>
2022-01-30 05:45:43 +0100

    fixes initial theme

--- contexts/Theme.tsx
+++ contexts/Theme.tsx
@@ -4,7 +4,6 @@ import {
   CustomThemeName,
   ThemeProvider as MuiThemeProvider
 } from '@iotabots/components'
-import { getItem, setItem } from '../utils/localStorage'
 
 export interface ThemeProviderProps {
   children: JSX.Element
@@ -23,29 +22,10 @@ export const ModeContext =
 const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
   const { children } = props
 
-  const [modeState, setModeState] = React.useState<Mode>('dark' as Mode)
-  const [themeState, setThemeState] =
-    React.useState<CustomThemeName | undefined>('light')
+  const [modeState] = React.useState<Mode>('dark' as Mode)
+  const [themeState] =
+    React.useState<CustomThemeName | undefined>('dark')
 
-  const getTheme = (mode: Mode): CustomThemeName | undefined => {
-    if (mode === 'light') return 'light'
-    return 'dark'
-  }
-
-  const toggleMode = (): void => {
-    const newMode = modeState === 'light' ? 'dark' : 'light'
-    setModeState(newMode as Mode)
-    setItem('mode', newMode)
-    setThemeState(getTheme(newMode as Mode))
-  }
-
-  React.useEffect(() => {
-    const localMode = getItem('mode')
-    if (localMode) {
-      setModeState(localMode as Mode)
-      setThemeState(getTheme(localMode as Mode))
-    }
-  }, [])
 
   return (
     <MuiThemeProvider
@@ -54,7 +34,6 @@ const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
       <ModeContext.Provider
         value={{
           mode: modeState,
-          toggleMode
         }}
       >
         <CssBaseline />
