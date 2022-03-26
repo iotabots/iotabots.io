a449455dcc89e9e8892476d43b62bee5d185a5ad
Ziegenpeter <ziegenpeter@localhost.localdomain>
2022-03-26 21:07:21 +0100

    Removed image wrapper

--- components/ImageLoader.tsx
+++ components/ImageLoader.tsx
@@ -12,9 +12,7 @@ interface Props{
 
 const Image: React.FC<Props> = ({ imageUrl, imageWidth, border_radius, border, imageHeight }) => {
         return <div>
-        <S.ImageWrapper>
             <S.Image src={imageUrl} style={{width: imageWidth, borderRadius: border_radius, border: border, height: imageHeight}}/>
-        </S.ImageWrapper>
     </div>
 }
 
