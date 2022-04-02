fb57ddf6b894bc6f678022910b014221a1386e92
Ziegenpeter <ziegenpeter@localhost.localdomain>
2022-04-02 15:24:31 +0200

    Added some more questions and answers to the FAQ

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -41,10 +41,18 @@ const FAQ: React.FC = () => (
           { }
         </ContentBox>
         <ContentBox
-          image='http://assets.iotabots.io/compressed/187.png'
+          image=''
           // eslint-disable-next-line max-len
-          text='Current IOTABOTS are deployed on the tesnet. The SaveTheBots initiative tries to make a successfull transition to the mainnet - so the users can hold their IOTABOTS.'
-          headline='What does SaveTheBots mean?'
+          text='Follow IOTABOTS on Twitter (https://twitter.com/iotabots) or/and join the Discord (https://t.co/BLwSLmwoJt) channel to not miss anything.'
+          headline='How do I know about any updates and giveaways?'
+        >
+          { }
+        </ContentBox>
+        <ContentBox
+          image=''
+          // eslint-disable-next-line max-len
+          text='On SOONAVERSE (https://soonaverse.com/) you can buy, sell and trade NFTs like the SOONABOTS without any fees. But it is also a platform to create DAOs and let the community engage for example via proposals.'
+          headline='What is SOONAVERSE?'
         >
           { }
         </ContentBox>
@@ -53,6 +61,14 @@ const FAQ: React.FC = () => (
           // eslint-disable-next-line max-len
           text='All currently existing IOTABOTS and SPACEBOTS were given away for free and can no longer be acquired. The sale of the SOONABOTS is still live on SOONAVERSE where you can get one for 100 MIOTA. Here is the link where you can buy some SOONABOTS: https://soonaverse.com/collection/0xeb47806ef8d4c908179bd05eeabc20bc3de8c81a'
           headline='How can I get NFTs?'
+        >
+          { }
+        </ContentBox>
+        <ContentBox
+          image='http://assets.iotabots.io/compressed/187.png'
+          // eslint-disable-next-line max-len
+          text='Current IOTABOTS are deployed on the tesnet. The SaveTheBots initiative tries to make a successfull transition to the mainnet - so the users can hold their IOTABOTS.'
+          headline='What does SaveTheBots mean?'
         >
           { }
         </ContentBox>
