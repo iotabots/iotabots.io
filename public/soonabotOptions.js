1b838fef47eca9af173886190967770599defdf7
Ziegenpeter <ziegenpeter@localhost.localdomain>
2022-04-03 13:09:52 +0200

    Added all Face options

--- public/soonabotOptions.js
+++ public/soonabotOptions.js
@@ -1,30 +1,30 @@
 export const displayOptions  = [
-    {value: 1.94,	name: '4 Star'	    	,optional: 'Rhombus'},
-    {value: 1.54,	name: '5 Star'	    	,optional: 'Rhombus'},
-    {value: 1.02,	name: 'Assembly'		,optional: 'Star   '},
-    {value: 2.97,	name: 'Atom'	    	,optional: 'Square '},
-    {value: 1.54,	name: 'Battery'	    	,optional: 'Rhombus'},
-    {value: 5.32,	name: 'Bee'	 	        ,optional: 'Circle '},
-    {value: 0.41,	name: 'BOLTS'	     	,optional: 'Sun    '},
-    {value: 3.99,	name: 'Cross'	    	,optional: 'Square '},
-    {value: 6.55,	name: 'Dizzy'	    	,optional: 'Circle '},
-    {value: 3.28,	name: 'Exclamation'	 	,optional: 'Square '},
-    {value: 6.55,	name: 'Eye'	 	        ,optional: 'Circle '},
-    {value: 7.57,	name: 'Gear'	    	,optional: 'Circle '},
-    {value: 6.55,	name: 'Hazard'	    	,optional: 'Circle '},
-    {value: 7.47,	name: 'Heart'	    	,optional: 'Circle '},
-    {value: 3.38,	name: 'Hornet'	 	    ,optional: 'Square '},
-    {value: 3.17,	name: 'IOTA'	    	,optional: 'Square '},
-    {value: 3.58,	name: 'LUM'	 	        ,optional: 'Square '},
-    {value: 6.24,	name: 'Notification'	,optional: 'Circle '},
-    {value: 6.86,	name: 'Pirate'	 	    ,optional: 'Circle '},
-    {value: 2.05,	name: 'Pulse'	 	    ,optional: 'Rhombus'},
-    {value: 3.28,	name: 'Pyramid'	 	    ,optional: 'Square '},
-    {value: 4.09,	name: 'Question'		,optional: 'Circle '},
-    {value: 2.76,	name: 'Shimmer'	 	    ,optional: 'Rhombus'},
-    {value: 1.02,	name: 'Strike'	 	    ,optional: 'Star   '},
-    {value: 2.76,	name: 'Target'	 	    ,optional: 'Rhombus'},
-    {value: 4.09,	name: 'Wasp'		    ,optional: 'Circle '},
+    {value: 1.94,	name: '4 Star'	    },	//,optional: 'Rhombus'
+    {value: 1.54,	name: '5 Star'	    },	//,optional: 'Rhombus'
+    {value: 1.02,	name: 'Assembly'	},	//,optional: 'Star'   
+    {value: 2.97,	name: 'Atom'	    },	//,optional: 'Square' 
+    {value: 1.54,	name: 'Battery'	    },	//,optional: 'Rhombus'
+    {value: 5.32,	name: 'Bee'	 	    },  //  ,optional: 'Circle' 
+    {value: 0.41,	name: 'BOLTS'	    }, 	//,optional: 'Sun'    
+    {value: 3.99,	name: 'Cross'	    },	//,optional: 'Square' 
+    {value: 6.55,	name: 'Dizzy'	    },	//,optional: 'Circle' 
+    {value: 3.28,	name: 'Exclamation'	}, 	//,optional: 'Square' 
+    {value: 6.55,	name: 'Eye'	 	    },  //  ,optional: 'Circle' 
+    {value: 7.57,	name: 'Gear'	    },	//,optional: 'Circle' 
+    {value: 6.55,	name: 'Hazard'	    },	//,optional: 'Circle' 
+    {value: 7.47,	name: 'Heart'	    },	//,optional: 'Circle' 
+    {value: 3.38,	name: 'Hornet'	 	},  //  ,optional: 'Square' 
+    {value: 3.17,	name: 'IOTA'	    },	//,optional: 'Square' 
+    {value: 3.58,	name: 'LUM'	 	    },  //  ,optional: 'Square' 
+    {value: 6.24,	name: 'Notification'},	//,optional: 'Circle' 
+    {value: 6.86,	name: 'Pirate'	 	},  //  ,optional: 'Circle' 
+    {value: 2.05,	name: 'Pulse'	 	},  //  ,optional: 'Rhombus'
+    {value: 3.28,	name: 'Pyramid'	 	},  //  ,optional: 'Square' 
+    {value: 4.09,	name: 'Question'	},	//,optional: 'Circle' 
+    {value: 2.76,	name: 'Shimmer'	 	},  //  ,optional: 'Rhombus'
+    {value: 1.02,	name: 'Strike'	 	},  //  ,optional: 'Star'   
+    {value: 2.76,	name: 'Target'	 	},  //  ,optional: 'Rhombus'
+    {value: 4.09,	name: 'Wasp'		},  //  ,optional: 'Circle' 
 ]
 
 export const bodyOptions  = [
@@ -34,9 +34,39 @@ export const bodyOptions  = [
 ]
 
 export const faceOptions  = [
-    { value: 0.33, name: 'Small' },
-    { value: 2, name: 'Medium' },
-    { value: 5.19, name: 'Large' },
+    {value: 0.49 ,name: '3D'        }, //,optional: 'Star'   
+    {value: 0.33 ,name: '1337'      }, //,optional: 'Star'   
+    {value: 1.56 ,name: 'Cry'       }, //,optional: 'Rhombus'
+    {value: 2.72 ,name: 'Dizzy'     }, //,optional: 'Square' 
+    {value: 2.63 ,name: 'Downcast'  }, //,optional: 'Square' 
+    {value: 2.80 ,name: 'Error'     }, //,optional: 'Square' 
+    {value: 4.69 ,name: 'Fear'      }, //,optional: 'Circle' 
+    {value: 3.37 ,name: 'Glaring'   }, //,optional: 'Square' 
+    {value: 5.10 ,name: 'Grinning'  }, //,optional: 'Circle' 
+    {value: 5.84 ,name: 'Happy'     }, //,optional: 'Circle' 
+    {value: 5.27 ,name: 'Kiss'      }, //,optional: 'Circle' 
+    {value: 2.96 ,name: 'Knockout'  }, //,optional: 'Square' 
+    {value: 2.96 ,name: 'Lol'       }, //,optional: 'Square' 
+    {value: 5.02 ,name: 'Love'      }, //,optional: 'Circle' 
+    {value: 4.94 ,name: 'Mask'      }, //,optional: 'Circle' 
+    {value: 3.54 ,name: 'Moneyface' }, //,optional: 'Circle' 
+    {value: 1.56 ,name: 'Music'     }, //,optional: 'Rhombus'
+    {value: 5.10 ,name: 'Rofl'      }, //,optional: 'Circle' 
+    {value: 2.63 ,name: 'Sad'       }, //,optional: 'Square' 
+    {value: 2.39 ,name: 'Shouting'  }, //,optional: 'Square' 
+    {value: 2.96 ,name: 'Shutup	'   }, //,optional: 'Square' 
+    {value: 2.88 ,name: 'Skull'     }, //,optional: 'Square' 
+    {value: 5.19 ,name: 'Sleeping'  }, //,optional: 'Rhombus'
+    {value: 3.05 ,name: 'Smiling'   }, //,optional: 'Square' 
+    {value: 2.39 ,name: 'Smirking'  }, //,optional: 'Square' 
+    {value: 5.35 ,name: 'Starstruck'},//,optional: 'Circle' 
+    {value: 2.55 ,name: 'Surprise'  }, //,optional: 'Square' 
+    {value: 1.73 ,name: 'Suspicious'},//,optional: 'Rhombus'
+    {value: 1.48 ,name: 'Sweat'     }, //,optional: 'Rhombus'
+    {value: 1.73 ,name: 'Tounge'    }, //,optional: 'Rhombus'
+    {value: 1.32 ,name: 'Vomit'     }, //,optional: 'Rhombus'
+    {value: 0.16 ,name: 'Woozy'     }, //,optional: 'Sun'    
+    {value: 3.29 ,name: 'Worried'   }, //,optional: 'Square' 
 ]
 
 export const backgroundOptions  = [
