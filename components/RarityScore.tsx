c2837483ea5af1ad77231957bc3b58213526a2c0
Ziegenpeter <ziegenpeter@localhost.localdomain>
2022-04-03 11:59:50 +0200

    Added iotacomponents - restrucring RarityScore

--- components/RarityScore.tsx
+++ components/RarityScore.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { useState } from 'react';
 import SelectSearch, { fuzzySearch } from 'react-select-search';
+import {
+    BaseLayout,
+    Container,
+    Grid,
+    Section,
+    SectionHeader,
+    Typography
+  } from '@iotabots/components'
 
 
 /**
@@ -8,41 +16,51 @@ import SelectSearch, { fuzzySearch } from 'react-select-search';
  * Required keys are "name" and "value" but you can have and use any number of key/value pairs.
  */
 
- const sizes = [
-                    { value: 's', name: 'Small' },
-                    { value: 'm', name: 'Medium' },
-                    { value: 'l', name: 'Large' },
-                ]
+//  const sizes = [
+//                     { value: 's', name: 'Small' },
+//                     { value: 'm', name: 'Medium' },
+//                     { value: 'l', name: 'Large' },
+//                 ]
 
-   const [size, setSize] = useState('s');
-     const style = {
-         fontFamily: '"Nunito Sans", sans-serif',
-         marginTop: '24px',
-         fontSize: '16px'
-     };
+//    const [size, setSize] = useState('s');
+//      const style = {
+//          fontFamily: '"Nunito Sans", sans-serif',
+//          marginTop: '24px',
+//          fontSize: '16px'
+//      };
 
-     if (size === 's') {
-         style.fontSize = '16px';
-     } else if (size === 'm') {
-         style.fontSize = '32px';
-     } if (size === 'l') {
-         style.fontSize = '64px';
-     }
+//      if (size === 's') {
+//          style.fontSize = '16px';
+//      } else if (size === 'm') {
+//          style.fontSize = '32px';
+//      } if (size === 'l') {
+//          style.fontSize = '64px';
+//      }
 
- const RarityScore: React.FC = () => {
+//  const RarityScore: React.FC = () => {
    
-     return(
-         <div>
-         <SelectSearch
-             options={sizes}
-             search
-             filterOptions={fuzzySearch}
-             emptyMessage="Not found"
-             placeholder="Select your country"
-         />
-         <h1 style={style}>Aa</h1>
-         </div>
-     )
-}
+//      return(
+//          <div>
+//          <SelectSearch
+//              options={sizes}
+//              search
+//              filterOptions={fuzzySearch}
+//              emptyMessage="Not found"
+//              placeholder="Select your country"
+//          />
+//          <h1 style={style}>Aa</h1>
+//          </div>
+//      )
+// }
 
-export default RarityScore
+export const RarityScoreSoonabots = () => {
+
+
+    return (       
+        <Container maxWidth='md'>
+
+        </Container>
+    );
+};
+
+export default RarityScoreSoonabots
