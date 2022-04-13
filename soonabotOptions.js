95cac9a8cb743799e3cf0354f6bc4a495ba9626b
huhn511 <huhn511@users.noreply.github.com>
2022-04-13 21:20:46 +0000

    deploy: 4b87628889a32307e65075c0b76db25f2ed351bb

---
+++ soonabotOptions.js
@@ -0,0 +1,268 @@
+// export const displayOptions  = [
+//     {value: 1.94,name: '4 Super Rare'},	//,raritystatus: 'Rare'
+//     {value: 1.54,name: '5 Super Rare'},	//,raritystatus: 'Rare'
+//     {value: 1.02,name: 'Assembly'},	//,raritystatus: 'Super Rare'   
+//     {value: 2.97,name: 'Atom'},	//,raritystatus: 'Uncommon' 
+//     {value: 1.54,name: 'Battery'},	//,raritystatus: 'Rare'
+//     {value: 5.32,name: 'Bee'},  //  ,raritystatus: 'Common' 
+//     {value: 0.41,name: 'BOLTS'}, 	//,raritystatus: 'Legendary'    
+//     {value: 3.99,name: 'Cross'},	//,raritystatus: 'Uncommon' 
+//     {value: 6.55,name: 'Dizzy'},	//,raritystatus: 'Common' 
+//     {value: 3.28,name: 'Exclamation'}, 	//,raritystatus: 'Uncommon' 
+//     {value: 6.55,name: 'Eye'},  //  ,raritystatus: 'Common' 
+//     {value: 7.57,name: 'Gear'},	//,raritystatus: 'Common' 
+//     {value: 6.55,name: 'Hazard'},	//,raritystatus: 'Common' 
+//     {value: 7.47,name: 'Heart'},	//,raritystatus: 'Common' 
+//     {value: 3.38,name: 'Hornet'},  //  ,raritystatus: 'Uncommon' 
+//     {value: 3.17,name: 'IOTA'},	//,raritystatus: 'Uncommon' 
+//     {value: 3.58,name: 'LUM'},  //  ,raritystatus: 'Uncommon' 
+//     {value: 6.24,name: 'Notification'},	//,raritystatus: 'Common' 
+//     {value: 6.86,name: 'Pirate'},  //  ,raritystatus: 'Common' 
+//     {value: 2.05,name: 'Pulse'},  //  ,raritystatus: 'Rare'
+//     {value: 3.28,name: 'Pyramid'},  //  ,raritystatus: 'Uncommon' 
+//     {value: 4.09,name: 'Question'},	//,raritystatus: 'Common' 
+//     {value: 2.76,name: 'Shimmer'},  //  ,raritystatus: 'Rare'
+//     {value: 1.02,name: 'Strike'},  //  ,raritystatus: 'Super Rare'   
+//     {value: 2.76,name: 'Target'},  //  ,raritystatus: 'Rare'
+//     {value: 4.09,name: 'Wasp'}  //  ,raritystatus: 'Common' 
+// ];
+
+export const displayOptions  = [
+    {value: 1.94,	label: '4 Star',		raritystatus: 'Rare' 		},
+    {value: 1.54,	label: '5 Star',		raritystatus: 'Rare' 		},
+    {value: 1.02,	label: 'Assembly',		raritystatus: 'Super Rare'	},   
+    {value: 2.97,	label: 'Atom',			raritystatus: 'Uncommon' 	},
+    {value: 1.54,	label: 'Battery',		raritystatus: 'Rare'		},
+    {value: 5.32,	label: 'Bee',			raritystatus: 'Common' 		},
+    {value: 0.41,	label: 'BOLTS',			raritystatus: 'Legendary'   }, 
+    {value: 3.99,	label: 'Cross',			raritystatus: 'Uncommon'	},
+    {value: 6.55,	label: 'Dizzy',			raritystatus: 'Common' 		},
+    {value: 3.28,	label: 'Exclamation',	raritystatus: 'Uncommon'	},
+    {value: 6.55,	label: 'Eye',			raritystatus: 'Common' 		},
+    {value: 7.57,	label: 'Gear',			raritystatus: 'Common' 		},
+    {value: 6.55,	label: 'Hazard',		raritystatus: 'Common' 		},
+    {value: 7.47,	label: 'Heart',			raritystatus: 'Common' 		},
+    {value: 3.38,	label: 'Hornet',		raritystatus: 'Uncommon' 	},
+    {value: 3.17,	label: 'IOTA',			raritystatus: 'Uncommon' 	},
+    {value: 3.58,	label: 'LUM',			raritystatus: 'Uncommon' 	},
+    {value: 6.24,	label: 'Notification',	raritystatus: 'Common' 		},
+    {value: 6.86,	label: 'Pirate',		raritystatus: 'Common' 		},
+    {value: 2.05,	label: 'Pulse',			raritystatus: 'Rare'		},
+    {value: 3.28,	label: 'Pyramid' ,		raritystatus: 'Uncommon' 	},
+    {value: 4.09,	label: 'Question',		raritystatus: 'Common' 		},
+    {value: 2.76,	label: 'Shimmer',		raritystatus: 'Rare'		},
+    {value: 1.02,	label: 'Strike' ,		raritystatus: 'Super Rare'  },
+    {value: 2.76,	label: 'Target',		raritystatus: 'Rare'		},
+    {value: 4.09,	label: 'Wasp',			raritystatus: 'Common' 		},
+];
+
+// export const bodyOptions  = [
+//     {value: 1.16, name: 'Chroma'    }, //,raritystatus: 'Rare'
+//     {value: 2.16, name: 'Cost'      }, //,raritystatus: 'Common '
+//     {value: 1.43, name: 'Cruise'    }, //,raritystatus: 'Uncommon '
+//     {value: 1.59, name: 'Dark'      }, //,raritystatus: 'Uncommon '
+//     {value: 2.46, name: 'Ebb'       }, //,raritystatus: 'Common '
+//     {value: 1.46, name: 'Flaky'     }, //,raritystatus: 'Uncommon '
+//     {value: 1.30, name: 'Frim'      }, //,raritystatus: 'Uncommon '
+//     {value: 1.13, name: 'Frog'      }, //,raritystatus: 'Rare'
+//     {value: 1.00, name: 'Froly'     }, //,raritystatus: 'Rare'
+//     {value: 1.13, name: 'Funk'      }, //,raritystatus: 'Uncommon '
+//     {value: 1.26, name: 'Gum'       }, //,raritystatus: 'Uncommon '
+//     {value: 1.69, name: 'Iri'       }, //,raritystatus: 'Uncommon '
+//     {value: 2.06, name: 'Lim'       }, //,raritystatus: 'Common '
+//     {value: 2.46, name: 'Lola'      }, //,raritystatus: 'Common '
+//     {value: 0.56, name: 'Manhatten' }, //,raritystatus: 'Rare'
+//     {value: 2.72, name: 'Mine'      }, //,raritystatus: 'Common '
+//     {value: 1.79, name: 'Moss'      }, //,raritystatus: 'Uncommon '
+//     {value: 1.93, name: 'Neon'      }, //,raritystatus: 'Uncommon '
+//     {value: 2.03, name: 'Pacific'   }, //,raritystatus: 'Common '
+//     {value: 1.96, name: 'Perlmutt'  }, //,raritystatus: 'Uncommon '
+//     {value: 2.03, name: 'Picton	'   }, //,raritystatus: 'Common '
+//     {value: 1.89, name: 'Pumice	'   }, //,raritystatus: 'Uncommon '
+//     {value: 1.86, name: 'Purple	'   }, //,raritystatus: 'Uncommon '
+//     {value: 1.96, name: 'Ran'       }, //,raritystatus: 'Uncommon '
+//     {value: 2.52, name: 'Ransom'    }, //,raritystatus: 'Common '
+//     {value: 2.76, name: 'Rescue'    }, //,raritystatus: 'Common '
+//     {value: 2.72, name: 'Ronchi'    }, //,raritystatus: 'Common '
+//     {value: 2.49, name: 'Ruff'      }, //,raritystatus: 'Common '
+//     {value: 0.83, name: 'Sahara'    }, //,raritystatus: 'Rare'
+//     {value: 1.99, name: 'Scheme'    }, //,raritystatus: 'Uncommon '
+//     {value: 1.23, name: 'Silver'    }, //,raritystatus: 'Uncommon '
+//     {value: 1.93, name: 'Sluck'     }, //,raritystatus: 'Uncommon '
+//     {value: 1.40, name: 'Spring'    }, //,raritystatus: 'Uncommon '
+//     {value: 2.66, name: 'Stripe'    }, //,raritystatus: 'Common '
+//     {value: 2.92, name: 'Twizz'     }, //,raritystatus: 'Common '
+//     {value: 1.33, name: 'Whity'     }, //,raritystatus: 'Uncommon '
+//     {value: 1.99, name: 'Xana'      }, //,raritystatus: 'Uncommon '
+//     {value: 2.66, name: 'Xil'       }, //,raritystatus: 'Common '
+//     {value: 2.03, name: 'Xux'       }, //,raritystatus: 'Common '
+//     {value: 2.36, name: 'Yach'      }, //,raritystatus: 'Common '
+//     {value: 2.82, name: 'Yeed'      }, //,raritystatus: 'Common '
+//     {value: 2.33, name: 'Yell'      }, //,raritystatus: 'Common '
+//     {value: 2.26, name: 'Yum'       }, //,raritystatus: 'Common '
+//     {value: 2.56, name: 'Zar'       }, //,raritystatus: 'Common '
+//     {value: 2.92, name: 'Zed'       }, //,raritystatus: 'Common '
+//     {value: 3.29, name: 'Zir'       }, //,raritystatus: 'Common '
+//     {value: 3.32, name: 'Zop'       }, //,raritystatus: 'Common '
+//     {value: 1.26, name: 'Zyan'      }, //,raritystatus: 'Uncommon '
+//     {value: 0.27, name: 'Zyr'       }, //,raritystatus: 'Super Rare   '
+// ];
+
+export const bodyOptions  = [
+    {value: 0.03, label: 'Azalea'	 ,raritystatus: 'Legendary'   },
+    {value: 0.07, label: 'Bank'		 ,raritystatus: 'Legendary'   },
+    {value: 0.10, label: 'Blush'	 ,raritystatus: 'Super Rare'  },
+    {value: 0.13, label: 'Bow'		 ,raritystatus: 'Super Rare'  },
+    {value: 0.17, label: 'Brilliant' ,raritystatus: 'Super Rare'  },
+    {value: 0.27, label: 'Camo'		 ,raritystatus: 'Super Rare'  },
+    {value: 0.33, label: 'Can'		 ,raritystatus: 'Super Rare'  },
+    {value: 0.50, label: 'Cashmere'	 ,raritystatus: 'Rare'  	  },
+    {value: 0.63, label: 'Chameleon' ,raritystatus: 'Rare'  	  },
+    {value: 0.86, label: 'Chamois'	 ,raritystatus: 'Rare'  	  },
+    {value: 1.03, label: 'Chenin'	 ,raritystatus: 'Rare'  	  },
+	
+	
+    {value: 1.16, label: 'Chroma'    ,raritystatus: 'Rare'        },
+    {value: 2.16, label: 'Cost'      ,raritystatus: 'Common'	  },
+    {value: 1.43, label: 'Cruise'    ,raritystatus: 'Uncommon'	  },
+    {value: 1.59, label: 'Dark'      ,raritystatus: 'Uncommon'	  },
+    {value: 2.46, label: 'Ebb'       ,raritystatus: 'Common'	  },
+    {value: 1.46, label: 'Flaky'     ,raritystatus: 'Uncommon'	  },
+    {value: 1.30, label: 'Frim'      ,raritystatus: 'Uncommon'	  },
+    {value: 1.13, label: 'Frog'      ,raritystatus: 'Rare'        },
+    {value: 1.00, label: 'Froly'     ,raritystatus: 'Rare'        },
+    {value: 1.13, label: 'Funk'      ,raritystatus: 'Uncommon'    },
+    {value: 1.26, label: 'Gum'       ,raritystatus: 'Uncommon'    },
+    {value: 1.69, label: 'Iri'       ,raritystatus: 'Uncommon' 	  },
+    {value: 2.06, label: 'Lim'       ,raritystatus: 'Common'   	  },
+    {value: 2.46, label: 'Lola'      ,raritystatus: 'Common'   	  },
+    {value: 0.56, label: 'Manhatten' ,raritystatus: 'Rare'        },
+    {value: 2.72, label: 'Mine'      ,raritystatus: 'Common'	  },
+    {value: 1.79, label: 'Moss'      ,raritystatus: 'Uncommon'    },
+    {value: 1.93, label: 'Neon'      ,raritystatus: 'Uncommon'    },
+    {value: 2.03, label: 'Pacific'   ,raritystatus: 'Common'      },
+    {value: 1.96, label: 'Perlmutt'  ,raritystatus: 'Uncommon'    },
+    {value: 2.03, label: 'Picton'    ,raritystatus: 'Common'      },
+    {value: 1.89, label: 'Pumice'    ,raritystatus: 'Uncommon'    },
+    {value: 1.86, label: 'Purple'    ,raritystatus: 'Uncommon'    },
+    {value: 1.96, label: 'Ran'       ,raritystatus: 'Uncommon'    },
+    {value: 2.52, label: 'Ransom'    ,raritystatus: 'Common'      },
+    {value: 2.76, label: 'Rescue'    ,raritystatus: 'Common'      },
+    {value: 2.72, label: 'Ronchi'    ,raritystatus: 'Common'      },
+    {value: 2.49, label: 'Ruff'      ,raritystatus: 'Common'      },
+    {value: 0.83, label: 'Sahara'    ,raritystatus: 'Rare'        },
+    {value: 1.99, label: 'Scheme'    ,raritystatus: 'Uncommon'    },
+    {value: 1.23, label: 'Silver'    ,raritystatus: 'Uncommon'    },
+    {value: 1.93, label: 'Sluck'     ,raritystatus: 'Uncommon'    },
+    {value: 1.40, label: 'Spring'    ,raritystatus: 'Uncommon'    },
+    {value: 2.66, label: 'Stripe'    ,raritystatus: 'Common'      },
+    {value: 2.92, label: 'Twizz'     ,raritystatus: 'Common'      },
+    {value: 1.33, label: 'Whity'     ,raritystatus: 'Uncommon'    },
+    {value: 1.99, label: 'Xana'      ,raritystatus: 'Uncommon'    },
+    {value: 2.66, label: 'Xil'       ,raritystatus: 'Common'      },
+    {value: 2.03, label: 'Xux'       ,raritystatus: 'Common'      },
+    {value: 2.36, label: 'Yach'      ,raritystatus: 'Common'      },
+    {value: 2.82, label: 'Yeed'      ,raritystatus: 'Common'      },
+    {value: 2.33, label: 'Yell'      ,raritystatus: 'Common'      },
+    {value: 2.26, label: 'Yum'       ,raritystatus: 'Common'      },
+    {value: 2.56, label: 'Zar'       ,raritystatus: 'Common'      },
+    {value: 2.92, label: 'Zed'       ,raritystatus: 'Common'      },
+    {value: 3.29, label: 'Zir'       ,raritystatus: 'Common'      },
+    {value: 3.32, label: 'Zop'       ,raritystatus: 'Common'      },
+    {value: 1.26, label: 'Zyan'      ,raritystatus: 'Uncommon'    },
+    {value: 0.27, label: 'Zyr'       ,raritystatus: 'Super Rare'  },
+];
+
+// export const faceOptions  = [
+//     {value: 0.49 ,name: '3D'        }, //,raritystatus: 'Super Rare'   
+//     {value: 0.33 ,name: '1337'      }, //,raritystatus: 'Super Rare'   
+//     {value: 1.56 ,name: 'Cry'       }, //,raritystatus: 'Rare'
+//     {value: 2.72 ,name: 'Dizzy'     }, //,raritystatus: 'Uncommon' 
+//     {value: 2.63 ,name: 'Downcast'  }, //,raritystatus: 'Uncommon' 
+//     {value: 2.80 ,name: 'Error'     }, //,raritystatus: 'Uncommon' 
+//     {value: 4.69 ,name: 'Fear'      }, //,raritystatus: 'Common' 
+//     {value: 3.37 ,name: 'Glaring'   }, //,raritystatus: 'Uncommon' 
+//     {value: 5.10 ,name: 'Grinning'  }, //,raritystatus: 'Common' 
+//     {value: 5.84 ,name: 'Happy'     }, //,raritystatus: 'Common' 
+//     {value: 5.27 ,name: 'Kiss'      }, //,raritystatus: 'Common' 
+//     {value: 2.96 ,name: 'Knockout'  }, //,raritystatus: 'Uncommon' 
+//     {value: 2.96 ,name: 'Lol'       }, //,raritystatus: 'Uncommon' 
+//     {value: 5.02 ,name: 'Love'      }, //,raritystatus: 'Common' 
+//     {value: 4.94 ,name: 'Mask'      }, //,raritystatus: 'Common' 
+//     {value: 3.54 ,name: 'Moneyface' }, //,raritystatus: 'Common' 
+//     {value: 1.56 ,name: 'Music'     }, //,raritystatus: 'Rare'
+//     {value: 5.10 ,name: 'Rofl'      }, //,raritystatus: 'Common' 
+//     {value: 2.63 ,name: 'Sad'       }, //,raritystatus: 'Uncommon' 
+//     {value: 2.39 ,name: 'Shouting'  }, //,raritystatus: 'Uncommon' 
+//     {value: 2.96 ,name: 'Shutup'    }, //,raritystatus: 'Uncommon' 
+//     {value: 2.88 ,name: 'Skull'     }, //,raritystatus: 'Uncommon' 
+//     {value: 5.19 ,name: 'Sleeping'  }, //,raritystatus: 'Rare'
+//     {value: 3.05 ,name: 'Smiling'   }, //,raritystatus: 'Uncommon' 
+//     {value: 2.39 ,name: 'Smirking'  }, //,raritystatus: 'Uncommon' 
+//     {value: 5.35 ,name: 'Super Rarestruck'},//,raritystatus: 'Common' 
+//     {value: 2.55 ,name: 'Surprise'  }, //,raritystatus: 'Uncommon' 
+//     {value: 1.73 ,name: 'Suspicious'},//,raritystatus: 'Rare'
+//     {value: 1.48 ,name: 'Sweat'     }, //,raritystatus: 'Rare'
+//     {value: 1.73 ,name: 'Tounge'    }, //,raritystatus: 'Rare'
+//     {value: 1.32 ,name: 'Vomit'     }, //,raritystatus: 'Rare'
+//     {value: 0.16 ,name: 'Woozy'     }, //,raritystatus: 'Legendary'    
+//     {value: 3.29 ,name: 'Worried'   }, //,raritystatus: 'Uncommon' 
+// ];
+
+export const faceOptions  = [
+    {value: 0.49 ,label: '3D'        ,raritystatus: 'Super Rare' 	}, 
+    {value: 0.33 ,label: '1337'      ,raritystatus: 'Super Rare' 	}, 
+    {value: 1.56 ,label: 'Cry'       ,raritystatus: 'Rare'       	},
+    {value: 2.72 ,label: 'Dizzy'     ,raritystatus: 'Uncommon'   	},
+    {value: 2.63 ,label: 'Downcast'  ,raritystatus: 'Uncommon'   	},
+    {value: 2.80 ,label: 'Error'     ,raritystatus: 'Uncommon'   	},
+    {value: 4.69 ,label: 'Fear'      ,raritystatus: 'Common'     	},
+    {value: 3.37 ,label: 'Glaring'   ,raritystatus: 'Uncommon'   	},
+    {value: 5.10 ,label: 'Grinning'  ,raritystatus: 'Common'     	},
+    {value: 5.84 ,label: 'Happy'     ,raritystatus: 'Common'     	},
+    {value: 5.27 ,label: 'Kiss'      ,raritystatus: 'Common'     	},
+    {value: 2.96 ,label: 'Knockout'  ,raritystatus: 'Uncommon'   	},
+    {value: 2.96 ,label: 'Lol'       ,raritystatus: 'Uncommon'   	},
+    {value: 5.02 ,label: 'Love'      ,raritystatus: 'Common'     	},
+    {value: 4.94 ,label: 'Mask'      ,raritystatus: 'Common'     	},
+    {value: 3.54 ,label: 'Moneyface' ,raritystatus: 'Common'     	},
+    {value: 1.56 ,label: 'Music'     ,raritystatus: 'Rare'       	},
+    {value: 5.10 ,label: 'Rofl'      ,raritystatus: 'Common'     	},
+    {value: 2.63 ,label: 'Sad'       ,raritystatus: 'Uncommon'   	},
+    {value: 2.39 ,label: 'Shouting'  ,raritystatus: 'Uncommon'   	},
+    {value: 2.96 ,label: 'Shutup'    ,raritystatus: 'Uncommon'   	},
+    {value: 2.88 ,label: 'Skull'     ,raritystatus: 'Uncommon'   	},
+    {value: 5.19 ,label: 'Sleeping'  ,raritystatus: 'Rare'       	},
+    {value: 3.05 ,label: 'Smiling'   ,raritystatus: 'Uncommon'   	},
+    {value: 2.39 ,label: 'Smirking'  ,raritystatus: 'Uncommon'  	},
+    {value: 5.35 ,label: 'Starsruck' ,raritystatus: 'Common'		},	
+    {value: 2.55 ,label: 'Surprise'  ,raritystatus: 'Uncommon'		},
+    {value: 1.73 ,label: 'Suspicious',raritystatus: 'Rare'			},
+    {value: 1.48 ,label: 'Sweat'     ,raritystatus: 'Rare'   		},
+    {value: 1.73 ,label: 'Tounge'    ,raritystatus: 'Rare'  		},
+    {value: 1.32 ,label: 'Vomit'     ,raritystatus: 'Rare'  		},
+    {value: 0.16 ,label: 'Woozy'     ,raritystatus: 'Legendary'		},   
+    {value: 3.29 ,label: 'Worried'   ,raritystatus: 'Uncommon'		},
+];
+
+// export const backgroundOptions  = [
+//     {value: 5.09 ,name:'Black'  }, //	raritystatus: 'Rare'
+//     {value: 8.56 ,name:'Blue'   }, //	raritystatus: 'Uncommon '
+//     {value:13.19 ,name:'Green'  }, //	raritystatus: 'Common '
+//     {value: 9.49 ,name:'Orange' }, //	raritystatus: 'Uncommon '
+//     {value:15.97 ,name:'Pink'   }, //	raritystatus: 'Common '
+//     {value:18.98 ,name:'Red'    }, //	raritystatus: 'Common '
+//     {value:15.51 ,name:'Sea'    }, //	raritystatus: 'Common '
+//     {value:13.19 ,name:'Yellow' }, //	raritystatus: 'Common '
+// ];
+
+export const backgroundOptions  = [
+    {value: 5.09 ,label:'Black'  ,raritystatus: 'Rare'		 },
+    {value: 8.56 ,label:'Blue'   ,raritystatus: 'Uncommon' 	 },
+    {value:13.19 ,label:'Green'  ,raritystatus: 'Common'	 },
+    {value: 9.49 ,label:'Orange' ,raritystatus: 'Uncommon'	 },
+    {value:15.97 ,label:'Pink'   ,raritystatus: 'Common' 	 },
+    {value:18.98 ,label:'Red'    ,raritystatus: 'Common' 	 },
+    {value:15.51 ,label:'Sea'    ,raritystatus: 'Common'	 },
+    {value:13.19 ,label:'Yellow' ,raritystatus: 'Common'	 },
+];
