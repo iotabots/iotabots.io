01f1fd236df128467f17797b1ce138a77b3febc1
huhn511 <sehe89@gmail.com>
2023-04-10 16:46:46 +0200

    support shimmerEVM testnet

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    RPC_URL_IOTA_EVM: 'https://evm.wasp.sc.iota.org',
+    RPC_URL_IOTA_EVM: 'https://json-rpc.evm.testnet.shimmer.network',
   },
   images: {
     loader: 'imgix',
--- utils/connectors.tsx
+++ utils/connectors.tsx
@@ -3,15 +3,15 @@ import { NetworkConnector } from '@web3-react/network-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 
 const RPC_URLS: { [chainId: number]: string } = {
-  1074: process.env.RPC_URL_IOTA_EVM as string,
+  1071: process.env.RPC_URL_IOTA_EVM as string,
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1074]
+  supportedChainIds: [1071]
 })
 
 export const network = new NetworkConnector({
-  urls: { 1074: RPC_URLS[1074] },
+  urls: { 1071: RPC_URLS[1071] },
   defaultChainId: 1
 })
 
